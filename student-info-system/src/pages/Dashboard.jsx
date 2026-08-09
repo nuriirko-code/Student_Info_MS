4915135c8d4799310db342c39660ed8d48ddadb7
Nuri <nuriirko@gmail.com>
2026-08-09 08:41:57 -0700

    commit

--- student-info-system/src/pages/Dashboard.jsx
+++ student-info-system/src/pages/Dashboard.jsx
@@ -1,6 +1,22 @@
 
 
+
 function Dashboard() {
+
+  const menuItems = [
+  { link: "#dash-home", icon: "fa-tachometer-alt", label: "Dashboard" },
+  { link: "#dash-add", icon: "fa-user-plus", label: "Add Student" },
+  { link: "#dash-view", icon: "fa-users", label: "View Students" },
+];
+
+
+const stats = [
+  { color: "blue", icon: "fa-users", label: "Total Students", value: "0" },
+  { color: "green", icon: "fa-male", label: "Male", value: "0" },
+  { color: "pink", icon: "fa-female", label: "Female", value: "0" },
+  { color: "orange", icon: "fa-chart-line", label: "CGPA", value: "0.00" },
+];
+
   return (
     <div className="dashboard-page">
       <nav className="sidebar">
@@ -8,9 +24,18 @@ function Dashboard() {
           <h2><i className="fas fa-graduation-cap"></i> SIMS</h2>
         </div>
         <ul className="sidebar-menu">
-          <li><a href="#dash-home"><i className="fas fa-tachometer-alt"></i> Dashboard</a></li>
-          <li><a href="#dash-add"><i className="fas fa-user-plus"></i> Add Student</a></li>
-          <li><a href="#dash-view"><i className="fas fa-users"></i> View Students</a></li>
+         
+           {menuItems.map((items, index) => (
+            <li key={index}>
+              <a href={items.link}>
+                <i className={`fas ${items.icon}`}></i> {items.label}
+              </a>
+            </li>
+          ) ) }
+        
+          
+
+          
         </ul>
       </nav>
 
@@ -22,13 +47,15 @@ function Dashboard() {
         <main className="dash-main">
           <section id="dash-home" className="dash-section">
             <h2 className="dash-title">Overview</h2>
-            <div className="stats-grid">
-              <div className="stat-card blue">
-                <i className="fas fa-users"></i>
-                <h3>Total Students</h3>
-                <p>0</p>
-              </div>
-            </div>
+          <div className="stats-grid">
+           {stats.map((item, index) => (
+    <div key={index} className={`stat-card ${item.color}`}>
+      <i className={`fas ${item.icon}`}></i>
+      <h3>{item.label}</h3>
+      <p>{item.value}</p>
+    </div>
+  ))}
+</div>
           </section>
         </main>
       </div>
